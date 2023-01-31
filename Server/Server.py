30ccef89bfaa3ae7e8eb14a261229801b339752c
Alexis Roques <alexisrqs@outlook.fr>
2023-01-31 20:20:51 +0100

    Working html file

--- Server/Server.py
+++ Server/Server.py
@@ -1,14 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.responses import FileResponse
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+def read_index():
+    return FileResponse("Server/index.html")
